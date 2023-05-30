0c057d1039fb96fecd9802d8475174f1311951d0
Yamna <yamna.bendacha@free.fr>
2023-05-30 15:36:31 +0200

    add items

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-class Items {
-    constructor(name, description, image, category) {
-        this.name = name;
-        this.description = description;
-        this.image = image;
-        this.category = category;
-    }
-}
-<<<<<<< HEAD
-=======
-let item11 = new item("https://boulanger.scene7.com/is/image/Boulanger/0818279025057_h_f_l_0?wid=500&hei=500", "GoPro Hero 9 Black", "Caméra polyvalente pour capturer des vidéos et des photos d'aventures sportives et en plein air.", "Electronique");
-let item12 = new item("https://www.nespresso.com/ecom/medias/sys_master/public/10910199316510/M-0510-VertuoPlus-C-Matt-Black-PDP-Background-Front.jpg?impolicy=productPdpSafeZone&imwidth=1238", "Nespresso Vertuo", "Machine à café automatique avec une large variété de capsules pour préparer de délicieux expressos.", "Cuisine");
-let item13 = new item("https://www.sony.fr/image/5d02da5df552836db894cead8a68f5f3?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF", "Sony WH-1000XM4", "Casque audio haut de gamme avec réduction de bruit active et une qualité sonore exceptionnelle.", "Électronique");
-let item14 = new item("https://m.media-amazon.com/images/I/81U7zKn3htL.jpg", "DJI Mavic Air 2", "Drone compact et puissant pour capturer des images aériennes et des vidéos de haute qualité", "Électronique");
->>>>>>> imene
-=======
 
-let item1 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "./assets/mcbook_pro.jpeg", "Electronique"); 
-let item2 = ("iPhone 12", "Téléphone haut de gamme avec un superbe écran et une caméra avancée.", "./assets/iphone12.jpg", "Electronique"); 
-let item3 = ("VTT Trek", "Vélo tout terrain robuste pour des aventures en montagne.", "https://www.bikelive.com/wp-content/uploads/2020/09/Trek-Supercaliber-9.9-9-scaled.jpg", "Sport"); 
-let item4 = ("Nikon D850", "Appareil photo p^rofessionnel avec une résolution exceptionnelle et de nombreuses fonctionnalités avancée.", "https://static.fnac-static.com/multimedia/Images/FR/NR/99/81/89/9011609/1520-1/tsp20170825094206/Reflex-Nikon-D850-Boitier-nu-Noir.jpg", "Electronique"); 
-let item5 = ("PlayStation 5", "Console de jeux de nouvelle génération offrant des graphismes impressionnants et une expérience de jeu immersive.", "https://www.cdiscount.com/pdt2/I/S/C/1/350x350/PS5DIGCHASSISC/rw/console-playstation-5-edition-digitale.jpg", "Jeux"); 
-let item6 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "https://www.cdiscount.com/pdt2/F/N/A/1/700x700/FPTR2FNA/rw/apple-macbook-pro-15-reconditionne-core-i7-2-8.jpg", "Electronique"); 
-let item7 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "https://www.cdiscount.com/pdt2/F/N/A/1/700x700/FPTR2FNA/rw/apple-macbook-pro-15-reconditionne-core-i7-2-8.jpg", "Electronique"); 
-let item8 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "https://www.cdiscount.com/pdt2/F/N/A/1/700x700/FPTR2FNA/rw/apple-macbook-pro-15-reconditionne-core-i7-2-8.jpg", "Electronique"); 
-let item9 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "https://www.cdiscount.com/pdt2/F/N/A/1/700x700/FPTR2FNA/rw/apple-macbook-pro-15-reconditionne-core-i7-2-8.jpg", "Electronique"); 
-let item10 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "https://www.cdiscount.com/pdt2/F/N/A/1/700x700/FPTR2FNA/rw/apple-macbook-pro-15-reconditionne-core-i7-2-8.jpg", "Electronique"); 
->>>>>>> 1140aea40a4891dbf1b2787bb82efe949ec875e2
+
+let item11 = new Items("./asset/0818279025057_h_f_l_0.webp", "GoPro Hero 9 Black", "Caméra polyvalente pour capturer des vidéos et des photos d'aventures sportives et en plein air.", "Electronique");
+let item12 = new Items(".asset/Nespresso Vertuo.avif", "Nespresso Vertuo", "Machine à café automatique avec une large variété de capsules pour préparer de délicieux expressos.", "Cuisine");
+let item13 = new Items(".asset/SONY WH 100 XM4.avif", "Sony WH-1000XM4", "Casque audio haut de gamme avec réduction de bruit active et une qualité sonore exceptionnelle.", "Electronique");
+let item14 = new Items(".asset/DJI MAVIC AIR 2.jpg", "DJI Mavic Air 2", "Drone compact et puissant pour capturer des images aériennes et des vidéos de haute qualité", "Electronique");
+let item15 = new Items(".asset/TheNorthFaceTerra65.webp", "The North Face Terra 65", "Sac à dos spacieux et confortable pour les randonnées de plusieurs jours en montagne.", "Sport");
+let item16 = new Items(".asset/Catan.avif", "Catan", "Jeu de stratégie populaire où les joueurs tentent de coloniser une île en collectant des ressources.", "Jeux");
+let item17 = new Items(".asset/irobotroomba980.jpg", "iRobot Roomba 980", "Robot aspirateur autonome qui nettoie les sols avec efficacité et programmable via une application.", "Electronique");
+let item18 = new Items(".asset/IpodTouch", "IPod Touch", "Lecteur de musique compact avec écran tactile et une grande capacité de stockage.", "Electronique");
+let item19 = new Items(".asset/ASUS ROG Swift.jpeg", "ASUS ROG Swift PG279Q", "Moniteur haute résolution avec un taux de rafraîchissement élevé pour une expérience de jeu fluide.", "Jeux");
+let item20 = new Items(".asset/Samsonite Spinner.jpeg", "Samsonite Spinner", "Valise durable et pratique avec des roulettes pour faciliter les déplacements.", "Transport");
