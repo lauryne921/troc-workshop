edcac2c9c2bd9d7fd4e9ad4c54984c08e7d59555
Lauryne <lauryne921@live.fr>
2023-05-30 13:04:48 +0200

    add first item

---
+++ script.js
@@ -0,0 +1,1 @@
+let item1 = ("MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "https://www.cdiscount.com/pdt2/F/N/A/1/700x700/FPTR2FNA/rw/apple-macbook-pro-15-reconditionne-core-i7-2-8.jpg", "Electronique"); 
