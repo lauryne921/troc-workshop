88a96c874d9eafcbf4ab7fe62b6e896bd0c9f7de
Lauryne <lauryne921@live.fr>
2023-05-31 17:44:18 +0200

    add detail.js

--- script.js
+++ script.js
@@ -18,30 +18,30 @@ function searchObject() {
     });
   }
 
-  searchObject();
+searchObject();
 
 
 
 
-    itemImage = itemsArray.map(item => {
-        const trocObject = document.querySelector('[data-troc-object]');
-        const card = trocObject.content.cloneNode(true);
-        const images = card.querySelector('.image');
-        const name = card.querySelector('.name');
-        const description = card.querySelector('.description');
-        const category = card.querySelector('.category');
+itemsArray.forEach(item => {
+    const trocObject = document.querySelector('[data-troc-object]');
+    const card = trocObject.content.cloneNode(true).children[0];
+    const images = card.querySelector('.image');
+    const name = card.querySelector('.name');
+    const description = card.querySelector('.description');
+    const category = card.querySelector('.category');
 
-        images.style.backgroundImage = `url('${item.image}')`; 
-        images.style.backgroundSize = 'cover';
-        images.style.backgroundRepeat = 'no-repeat';
-        category.textContent = `${item.category}`; 
-        name.textContent = `${item.name}`; 
-        description.textContent = `${item.description}`;
+    images.style.backgroundImage = `url('${item.image}')`; 
+    images.style.backgroundSize = 'cover';
+    images.style.backgroundRepeat = 'no-repeat';
+    category.textContent = `${item.category}`; 
+    name.textContent = `${item.name}`; 
+    description.textContent = `${item.description}`;
 
-        test.append(card);
+    test.append(card);
 
-        card.addEventListener('click', (event) => {
-            event.stopPropagation();
-            console.log('hello'); 
+    card.addEventListener('click', () => {
+        window.location.href = './detail.html';
+        localStorage.setItem('nameObject', item.name);
     });
 });
