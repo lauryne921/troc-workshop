fab32c8aa903f3afcd6d398150a602dc81d5dc5f
Lauryne <lauryne921@live.fr>
2023-05-31 18:37:56 +0200

    add detail.js

--- home.js
+++ home.js
@@ -1,3 +1,38 @@
+const searchBar = document.getElementById('search-bar');
+searchBar.addEventListener('input', handleSearch);
+
+function handleSearch() {
+    const searchTerm = searchBar.value.toLowerCase();
+    const searchResults = itemsArray.filter(item => {
+        const itemName = item.name.toLowerCase();
+        const itemCategory = item.category.toLowerCase();
+        return itemName.includes(searchTerm) || itemCategory.includes(searchTerm);
+    });
+    
+    displaySearchResults(searchResults);
+}
+
+function displaySearchResults(results) {  
+    test.innerHTML = '';
+  
+    results.forEach(item => {
+        const resultItem = document.createElement('div');
+      
+        const imageElement = document.createElement('img');
+        imageElement.src = item.image;
+        imageElement.alt = item.name;
+        imageElement.classList.add('image');
+        resultItem.appendChild(imageElement);
+      
+        const nameElement = document.createElement('h4');
+        nameElement.textContent = item.name;
+        resultItem.appendChild(nameElement);
+      
+        test.appendChild(resultItem);
+    });
+}
+
+
 let itemImage; 
 const test = document.getElementById('test');
 itemImage = itemsArray.slice(0, 5).map(item => {
@@ -5,11 +40,13 @@ itemImage = itemsArray.slice(0, 5).map(item => {
     const card = trocObject.content.cloneNode(true).children[0];
     const images = card.querySelector('.image');
     const name= card.querySelector('.name');
+    const category = card.querySelector('.category');
 
     images.style.backgroundImage = `url('${item.image}')`; 
     images.style.backgroundSize = 'cover';
     images.style.backgroundRepeat = 'no-repeat';
     name.textContent = `${item.name}`; 
+    category.textContent = `${item.category}`;
 
     test.append(card);
 
@@ -25,11 +62,13 @@ itemImage = itemsArray.slice(6, 11).map(item => {
     const card = trocObject.content.cloneNode(true).children[0];
     const images = card.querySelector('.image');
     const name= card.querySelector('.name');
+    const category = card.querySelector('.category');
 
     images.style.backgroundImage = `url('${item.image}')`; 
     images.style.backgroundSize = 'cover';
     images.style.backgroundRepeat = 'no-repeat';
     name.textContent = `${item.name}`; 
+    category.textContent = `${item.category}`;
 
     test2.append(card);
 
