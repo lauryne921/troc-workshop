82840bf6bc6118c8c2af22456decf09bfa1899ee
Lauryne <lauryne921@live.fr>
2023-05-31 12:31:29 +0200

    add data.js

---
+++ data.js
@@ -0,0 +1,76 @@
+class Items {
+    constructor(image, name, description, category) {
+        this.image = image;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+}
+
+let item1 = new Items("./assets/mcbook_pro.jpeg", "MacBook Pro", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "Electronique"); 
+let item2 = new Items("./assets/iphone12.jpg", "iPhone 12", "Téléphone haut de gamme avec un superbe écran et une caméra avancée.", "Electronique"); 
+let item3 = new Items("./assets/vtt.jpg", "VTT Trek", "Vélo tout terrain robuste pour des aventures en montagne.", "Sport"); 
+let item4 = new Items("./assets/nikon.jpg", "Nikon D850", "Appareil photo p^rofessionnel avec une résolution exceptionnelle et de nombreuses fonctionnalités avancée.", "Electronique"); 
+let item5 = new Items("./assets/ps5.jpg", "PlayStation 5", "Console de jeux de nouvelle génération offrant des graphismes impressionnants et une expérience de jeu immersive.", "Jeux"); 
+let item6 = new Items("./assets/apple_watch.jpg", "Apple Watch Series 6", "Montre connectée avec suivi de la condition physique, notifications et fonctionnalités avancées.", "Electronique"); 
+let item7 = new Items("./assets/ipad_pro.jpg", "iPad Pro", "Tablette haut de gamme avec un écran Retina et des performances exceptionnelles.", "Electronique"); 
+let item8 = new Items("./assets/guitare.jpg", "Guitare acoustique", "Guitare en bois de qualité supérieure avec un son riche et une belle finition.", "Musique"); 
+let item9 = new Items("./assets/scooter.jpg", "Xiaomi Mi Electric Scooter", "Ordinateur portable puissant avec écran Retina et processeur rapide.", "Electronique"); 
+let item10 = new Items("./assets/jbl.jpg", "JBL Charge 4", "Enceinte portable étanche avec un son puissant et une longue autonomie de batterie.", "Electronique"); 
+
+let item11 = new Items("./assets/0818279025057_h_f_l_0.webp", "GoPro Hero 9 Black", "Caméra polyvalente pour capturer des vidéos et des photos d'aventures sportives et en plein air.", "Electronique");
+let item12 = new Items("./assets/Nespresso Vertuo.avif", "Nespresso Vertuo", "Machine à café automatique avec une large variété de capsules pour préparer de délicieux expressos.", "Cuisine");
+let item13 = new Items("./assets/SONY WH 100 XM4.avif", "Sony WH-1000XM4", "Casque audio haut de gamme avec réduction de bruit active et une qualité sonore exceptionnelle.", "Electronique");
+let item14 = new Items("./assets/DJI MAVIC AIR 2.jpg", "DJI Mavic Air 2", "Drone compact et puissant pour capturer des images aériennes et des vidéos de haute qualité", "Electronique");
+let item15 = new Items("./assets/TheNorthFaceTerra65.webp", "The North Face Terra 65", "Sac à dos spacieux et confortable pour les randonnées de plusieurs jours en montagne.", "Sport");
+let item16 = new Items("./assets/Catan.avif", "Catan", "Jeu de stratégie populaire où les joueurs tentent de coloniser une île en collectant des ressources.", "Jeux");
+let item17 = new Items("./assets/irobotroomba980.jpg", "iRobot Roomba 980", "Robot aspirateur autonome qui nettoie les sols avec efficacité et programmable via une application.", "Electronique");
+let item18 = new Items("./assets/IpodTouch.jpg", "IPod Touch", "Lecteur de musique compact avec écran tactile et une grande capacité de stockage.", "Electronique");
+let item19 = new Items("./assets/ASUS ROG Swift.jpeg", "ASUS ROG Swift PG279Q", "Moniteur haute résolution avec un taux de rafraîchissement élevé pour une expérience de jeu fluide.", "Jeux");
+let item20 = new Items("./assets/Samsonite Spinner.jpeg", "Samsonite Spinner", "Valise durable et pratique avec des roulettes pour faciliter les déplacements.", "Transport");
+
+let Item21 = new Items("./assets/plancha-electrique.jpg", "Plancha électrique", "Plancha électrique pour griller et cuisiner des aliments de manière saine et savoureuse.", "cuisine");
+let Item22 = new Items("./assets/appareil-fitness.jpg", "Appareil de fitness", "Appareil d'exercice pour le cardio-training et le renforcement musculaire à domicile.", "Sport");
+let Item23 = new Items("./assets/sac-a-main.jpg", "Sac à main", "Sac à main élégant et intemporel en cuir de haute qualité.", "Esthétique");
+let Item24 = new Items("./assets/imprimante-mutifonction.png", "Imprimante multifonction", "Imprimante, scanner et photocopieur sans cartouches d'encre, avec une grande capacité de réservoir.", "Electronique");
+let Item25 = new Items("./assets/table-ping-pong.jpg", "Table de ping-pong", "Table de ping-pong de qualité professionnelle pour des parties amusantes en intérieur ou en extérieur.", "Sport");
+let Item26 = new Items("./assets/kit-peinture-artistique.webp", "Kit de peinture artistique", "Ensemble complet de peintures, pinceaux et toiles pour les amateurs d'art.", "Art");
+let Item27 = new Items("./assets/parfum-luxe.jpg", "Parfum de luxe", "Parfum féminin élégant et raffiné avec des notes florales et orientales.", "Esthétique");
+let Item28 = new Items("./assets/talkie-walkie.avif", "Talkie-walkie", "Talkie-walkie étanche pour une communication fiable lors des activités en plein air.", "Electronique");
+let Item29 = new Items("./assets/enceinte-intelligente.png", "Enceinte intelligente", "Enceinte connectée avec commande vocale et intégration d'assistants virtuels.", "Electronique");
+let Item30 = new Items("./assets/zelda-botw.jpg", "Jeu vidéo", "Jeu d'aventure épique dans un vaste monde ouvert, rempli d'exploration et de défis.", "Jeu");
+
+
+
+let itemsArray = [
+    item1, 
+    item2,
+    item3,
+    item4,
+    item5,
+    item6,
+    item7,
+    item8,
+    item9,
+    item10,
+    item11,
+    item12,
+    item13,
+    item14,
+    item15,
+    item16,
+    item17,
+    item18,
+    item19,
+    item20,
+    Item21,
+    Item22,
+    Item23,
+    Item24,
+    Item25,
+    Item26,
+    Item27,
+    Item28,
+    Item29,
+    Item30
+];
